c915bb1c2e557f19c24679a1bfbb1e317c31de09
Pierre Mailhot <pierre.mailhot@gmail.com>
2016-06-20 14:13:13 -0400

    Update urls.py
    Adding a comment

--- ecommerce/extensions/payment/urls.py
+++ ecommerce/extensions/payment/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 
 from ecommerce.extensions.payment import views
 
+# Added by EDUlib, added a line for netbanx_notify
 urlpatterns = [
     url(r'^netbanx/notify/page.html$', views.NetbanxNotifyView.as_view(), name='netbanx_notify'),
     url(r'^cybersource/notify/$', views.CybersourceNotifyView.as_view(), name='cybersource_notify'),
