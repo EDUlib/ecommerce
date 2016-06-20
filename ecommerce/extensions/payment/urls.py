7467f41f158cc2fd2a2314d3c2bbe24d14cbf0ec
Pierre Mailhot <pierre.mailhot@gmail.com>
2016-06-20 14:13:13 -0400

    Update urls.py
    Adding a comment

--- ecommerce/extensions/payment/urls.py
+++ ecommerce/extensions/payment/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 from ecommerce.extensions.payment.views import cybersource, PaymentFailedView
 from ecommerce.extensions.payment.views.paypal import PaypalPaymentExecutionView, PaypalProfileAdminView
 
+# Added by EDUlib, added a line for netbanx_notify
 urlpatterns = [
     url(r'^paysafe/notify/page.html$', views.PaysafeNotifyView.as_view(), name='paysafe_notify'),
     url(r'^cybersource/notify/$', cybersource.CybersourceNotifyView.as_view(), name='cybersource_notify'),
