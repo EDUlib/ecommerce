c5710ba08131790ec08d0959fb8598614f514203
Pierre Mailhot <pierre.mailhot@gmail.com>
2016-06-20 13:23:31 -0400

    Update 0006_enable_payment_processors.py
    Added Netbanx to the switches

--- ecommerce/extensions/payment/migrations/0006_enable_payment_processors.py
+++ ecommerce/extensions/payment/migrations/0006_enable_payment_processors.py
@@ -6,14 +6,17 @@ from django.db import migrations, models
 
 from ecommerce.extensions.payment.processors.cybersource import Cybersource
 from ecommerce.extensions.payment.processors.paypal import Paypal
-
+# Added by EDUlib
+from ecommerce.extensions.payment.processors.netbanx import Netbanx
+# Added by EDUlib
 
 def enable_payment_processors(apps, schema_editor):
     """
     Enable both existing payment processors.
     """
     Switch = apps.get_model('waffle', 'Switch')
-    for processor in (Cybersource, Paypal):
+    # Modified by EDUlib, Netbanx added
+    for processor in (Cybersource, Paypal, Netbanx):
         Switch(name=settings.PAYMENT_PROCESSOR_SWITCH_PREFIX + processor.NAME, active=True).save()
 
 
@@ -22,7 +25,8 @@ def delete_processor_switches(apps, schema_editor):
     Remove payment processor switches.
     """
     Switch = apps.get_model('waffle', 'Switch')
-    for processor in (Cybersource, Paypal):
+    # Modified by EDUlib, Netbanx added
+    for processor in (Cybersource, Paypal, Netbanx):
         Switch.objects.get(name=settings.PAYMENT_PROCESSOR_SWITCH_PREFIX + processor.NAME).delete()
 
 
