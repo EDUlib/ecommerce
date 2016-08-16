a80738f2a0b1458a60324ed8812fc38eeb73c6af
Ubuntu <ubuntu@ip-10-0-0-71.ec2.internal>
2016-08-16 14:12:19 -0400

    Security patch does not work for us Problem with launching the server

--- ecommerce/extensions/app.py
+++ ecommerce/extensions/app.py
@@ -1,16 +1,7 @@
 from oscar import app
-from oscar.core.application import Application
-
 
 class EdxShop(app.Shop):
     # URLs are only visible to users with staff permissions
     default_permissions = 'is_staff'
-
-    # Override core app instances with blank application instances to exclude their URLs.
-    promotions_app = Application()
-    catalogue_app = Application()
-    offer_app = Application()
-    search_app = Application()
-
 
 application = EdxShop()
