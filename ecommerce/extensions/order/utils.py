889d4c54648cecad79274806066ed60dae2f9d84
Pierre Mailhot <pierremailhot@imacdeastrateur.fil.umontreal.ca>
2016-08-23 15:27:31 -0400

    Resetting OFFSET to 100000 for Production

--- ecommerce/extensions/order/utils.py
+++ ecommerce/extensions/order/utils.py
@@ -16,7 +16,8 @@ Order = get_model('order', 'Order')
 class OrderNumberGenerator(object):
     # Modified by EDUlib
     #OFFSET = 100000
-    OFFSET = 500000
+    #OFFSET = 500000
+    OFFSET = 100000
     # Modified by EDUlib
 
     def order_number(self, basket):
