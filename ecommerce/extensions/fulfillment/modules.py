0d597e13131cf3977325f23cf7ffa8c05a7a3f35
Ubuntu <ubuntu@ip-10-0-0-231.ca-central-1.compute.internal>
2017-01-23 17:31:42 -0500

    Changes needed in order to send an email receipt from EDUlib

--- ecommerce/extensions/fulfillment/modules.py
+++ ecommerce/extensions/fulfillment/modules.py
@@ -26,6 +26,8 @@ from ecommerce.extensions.voucher.models import OrderLineVouchers
 from ecommerce.extensions.voucher.utils import create_vouchers
 from ecommerce.notifications.notifications import send_notification
 
+from decimal import *
+
 Benefit = get_model('offer', 'Benefit')
 Product = get_model('catalogue', 'Product')
 Range = get_model('offer', 'Range')
@@ -247,6 +249,7 @@ class EnrollmentFulfillmentModule(BaseFulfillmentModule):
                 )
                 line.set_status(LINE.FULFILLMENT_TIMEOUT_ERROR)
         logger.info("Finished fulfilling 'Seat' product types for order [%s]", order.number)
+        self.send_email(order, course_key)
         return order, lines
 
     def revoke_line(self, line):
@@ -294,6 +297,38 @@ class EnrollmentFulfillmentModule(BaseFulfillmentModule):
 
         return False
 
+    def send_email(self, order, course_key):
+        """ Sends an email with enrollment code order information. """
+        # Note (multi-courses): Change from a course_name to a list of course names.
+        product = order.lines.first().product
+        course = Course.objects.get(id=product.attr.course_key)
+
+        course_no_taxes = Decimal(order.total_incl_tax) / Decimal(1.14975)
+        tps_taxes = Decimal(order.total_incl_tax) / Decimal(1.14975) * Decimal(0.05)
+        tvq_taxes = Decimal(order.total_incl_tax) / Decimal(1.14975) * Decimal(0.09975)
+
+        course_no_taxes = "%.2f" % course_no_taxes
+        tps_taxes = "%.2f" % tps_taxes
+        tvq_taxes = "%.2f" % tvq_taxes
+
+        send_notification(
+            order.user,
+            'COURSE_PURCHASED',
+            context={
+                'contact_url': order.site.siteconfiguration.build_lms_url('/contact'),
+                'course_title': course_key,
+                'course_price': "%.2f" % Decimal(order.total_incl_tax),
+                'order_number': order.number,
+                'order_currency': order.currency,
+                'partner_name': order.site.siteconfiguration.partner.name,
+                'lms_url': order.site.siteconfiguration.build_lms_url(),
+                'receipt_page_url': order.site.siteconfiguration.build_lms_url('{}?orderNum={}'.format(settings.RECEIPT_PAGE_PATH, order.number)),
+                'course_no_taxes': course_no_taxes,
+                'tps': tps_taxes,
+                'tvq': tvq_taxes,
+            },
+            site=order.site
+        )
 
 class CouponFulfillmentModule(BaseFulfillmentModule):
     """ Fulfillment Module for coupons. """
@@ -447,24 +482,54 @@ class EnrollmentCodeFulfillmentModule(BaseFulfillmentModule):
         # Note (multi-courses): Change from a course_name to a list of course names.
         product = order.lines.first().product
         course = Course.objects.get(id=product.attr.course_key)
+
+        logger.info("AVANT LE CALCUL DES TAXES")
+        course_no_taxes = Decimal(order.total_incl_tax) / Decimal(1.14975)
+        tps_taxes = Decimal(order.total_incl_tax) / Decimal(1.14975) * Decimal(0.05)
+        tvq_taxes = Decimal(order.total_incl_tax) / Decimal(1.14975) * Decimal(0.09975)
+
+        course_no_taxes = "%.2f" % course_no_taxes
+        tps_taxes = "%.2f" % tps_taxes
+        tvq_taxes = "%.2f" % tvq_taxes
+
         receipt_page_url = get_receipt_page_url(
             order_number=order.number,
             site_configuration=order.site.siteconfiguration
         )
+        #send_notification(
+        #    order.user,
+        #    'ORDER_WITH_CSV',
+        #    context={
+        #        'contact_url': order.site.siteconfiguration.build_lms_url('/contact'),
+        #        'course_name': course.name,
+        #        'download_csv_link': order.site.siteconfiguration.build_ecommerce_url(
+        #            reverse('coupons:enrollment_code_csv', args=[order.number])
+        #        ),
+        #        'enrollment_code_title': product.title,
+        #        'lms_url': order.site.siteconfiguration.build_lms_url(),
+        #        'order_number': order.number,
+        #        'partner_name': order.site.siteconfiguration.partner.name,
+        #        'receipt_page_url': receipt_page_url,
+        #    },
+        #    site=order.site
+        #)
+        logger.info("AVANT send_notification")
         send_notification(
             order.user,
-            'ORDER_WITH_CSV',
+            'COURSE_PURCHASED',
             context={
                 'contact_url': order.site.siteconfiguration.build_lms_url('/contact'),
-                'course_name': course.name,
-                'download_csv_link': order.site.siteconfiguration.build_ecommerce_url(
-                    reverse('coupons:enrollment_code_csv', args=[order.number])
-                ),
-                'enrollment_code_title': product.title,
-                'lms_url': order.site.siteconfiguration.build_lms_url(),
+                'course_title': course_key,
+                'course_price': "%.2f" % Decimal(order.total_incl_tax),
                 'order_number': order.number,
+                'order_currency': order.currency,
                 'partner_name': order.site.siteconfiguration.partner.name,
-                'receipt_page_url': receipt_page_url,
+                'lms_url': order.site.siteconfiguration.build_lms_url(),
+                'receipt_page_url': order.site.siteconfiguration.build_lms_url('{}?orderNum={}'.format(settings.RECEIPT_PAGE_PATH, order.number)),
+                'course_no_taxes': course_no_taxes,
+                'tps': tps_taxes,
+                'tvq': tvq_taxes,
             },
             site=order.site
         )
+        logger.info("APRES send_notification")
