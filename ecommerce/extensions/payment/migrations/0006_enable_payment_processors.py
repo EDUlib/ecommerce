b8b11d3c77dd60eb860ac2794b657dd1d2d32ef0
Ubuntu <ubuntu@ip-10-0-0-231.ca-central-1.compute.internal>
2017-01-23 13:14:15 -0500

    Adding changes for ficus-edulib. Cleaning up traces.

--- ecommerce/extensions/payment/migrations/0006_enable_payment_processors.py
+++ ecommerce/extensions/payment/migrations/0006_enable_payment_processors.py
@@ -6,6 +6,10 @@ from django.db import migrations, models
 
 from ecommerce.extensions.payment.processors.cybersource import Cybersource
 from ecommerce.extensions.payment.processors.paypal import Paypal
+# Added by EDUlib
+from ecommerce.extensions.payment.processors.paysafe import Paysafe
+# Added by EDUlib
+
 
 
 def enable_payment_processors(apps, schema_editor):
@@ -13,7 +17,8 @@ def enable_payment_processors(apps, schema_editor):
     Enable both existing payment processors.
     """
     Switch = apps.get_model('waffle', 'Switch')
-    for processor in (Cybersource, Paypal):
+    # Modified by EDUlib, Paysafe added
+    for processor in (Cybersource, Paypal, Paysafe):
         Switch(name=settings.PAYMENT_PROCESSOR_SWITCH_PREFIX + processor.NAME, active=True).save()
 
 
@@ -22,7 +27,8 @@ def delete_processor_switches(apps, schema_editor):
     Remove payment processor switches.
     """
     Switch = apps.get_model('waffle', 'Switch')
-    for processor in (Cybersource, Paypal):
+    # Modified by EDUlib, Paysafe added
+    for processor in (Cybersource, Paypal, Paysafe):
         Switch.objects.get(name=settings.PAYMENT_PROCESSOR_SWITCH_PREFIX + processor.NAME).delete()
 
 
