c1e21d7fb5e472d178072e6e3b2363405f66841a
Ubuntu <ubuntu@ip-10-0-0-100.ca-central-1.compute.internal>
2017-08-07 19:04:24 -0400

    Changes needed by Ginkgo Refund still not reaching Paysafe

--- ecommerce/extensions/order/utils.py
+++ ecommerce/extensions/order/utils.py
@@ -29,9 +29,10 @@ RefundLine = get_model('refund', 'RefundLine')
 
 class OrderNumberGenerator:
     # Modified by EDUlib
-    OFFSET = 100000
+    #OFFSET = 100000
     #OFFSET = 500000
     #OFFSET = 600000
+    OFFSET = 700000
     # Modified by EDUlib
 
     def order_number(self, basket):
