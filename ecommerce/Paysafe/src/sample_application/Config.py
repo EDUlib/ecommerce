5667d41b9bbea35b3e9a3f796ad7491f9a0dad1e
sambapete <pierre.mailhot@gmail.com>
2019-12-11 11:52:45 -0500

    Adding Paysafe Python 3 SDK directly under ecommerce repository

---
+++ ecommerce/Paysafe/src/sample_application/Config.py
@@ -0,0 +1,16 @@
+'''
+Created on Feb 25, 2015
+
+@author: amol.mithari
+'''
+
+class Config(object):
+    '''
+    classdocs
+    '''
+
+    api_key = 'devcentre4628'
+    api_password =  'B-qa2-0-548ef25d-302b0213119f70d83213f828bc442dfd0af3280a7b48b1021400972746f9abe438554699c8fa3617063ca4c69a'
+    account_number = '89983472'
+    environment = "TEST"
+
