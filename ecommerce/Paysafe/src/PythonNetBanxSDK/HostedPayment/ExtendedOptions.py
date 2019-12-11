82d56764133689c51b08918fedaf20102041e302
sambapete <pierre.mailhot@gmail.com>
2019-12-11 11:52:45 -0500

    Adding Paysafe Python 3 SDK directly under ecommerce repository

---
+++ ecommerce/Paysafe/src/PythonNetBanxSDK/HostedPayment/ExtendedOptions.py
@@ -0,0 +1,37 @@
+'''
+Created on 05-Feb-2015
+
+@author: Asawari.Vaidya
+'''
+
+# EDUlib Python 2
+#####from PythonNetBanxSDK.common.DomainObject import DomainObject
+
+# EDUlib Python 3
+from ecommerce.Paysafe.src.PythonNetBanxSDK.common.DomainObject import DomainObject
+
+class ExtendedOptions(DomainObject):
+    '''
+    classdocs
+    '''
+    def __init__(self, obj):
+        '''
+        Constructor
+        '''
+        
+        if obj is not None:
+            self.setProperties(obj)
+        else:
+            pass
+                        
+    '''
+    Property Key
+    '''   
+    def key(self, key):
+        self.__dict__['key'] = key
+        
+    '''
+    Property Value
+    '''   
+    def value(self, value):
+        self.__dict__['value'] = value
